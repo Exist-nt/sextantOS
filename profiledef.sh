53d92a2295966e58d477a72b4d35c1254cb64724
Existnt <teddy.bridemayor@gmail.com>
2026-03-15 17:55:07 +0000

    Add missing /root/ folder

--- profiledef.sh
+++ profiledef.sh
@@ -17,7 +17,7 @@ bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--lon
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/etc/gshadow"]="0:0:400"
-  ["/root/.config/sway/scripts/*"]="0:0:777"
+  ["/etc/skel/.config/sway/scripts"]="0:0:777"
   ["/root"]="0:0:750"
   ["/etc/ly/setup.sh"]="0:0:777"
   ["/root/.automated_script.sh"]="0:0:755"
